5a02ab5355720ab4d3602b707e905c4b864f78dc
cdanansuriya <cdanansuriya.study@gmail.com>
2021-03-23 17:08:24 +0530

    remover testing

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,5 @@ mod selector;
 fn main() {
     let player_guess = player::guess();
     let machine_guess = machine::guess();
-
-    println!("Player's guess: {}", player_guess);
-    println!("Machine's guess: {}", machine_guess);
-
     selector::get_decison(&player_guess, &machine_guess);
 }
