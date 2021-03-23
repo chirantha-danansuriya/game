b95440e37bc9a8cd1a3e321fea9d48a66555966f
cdanansuriya <cdanansuriya.study@gmail.com>
2021-03-23 12:54:59 +0530

    User mod verfy input is <=100

--- src/player.rs
+++ src/player.rs
@@ -1,9 +1,16 @@
 use std::io::stdin;
 
 pub fn guess() -> u32 {
-    println!("Please guess the number bitween 1 and 100: ");
+    println!("Please guess the number between 1 and 100: ");
     let mut user_input: String = String::new();
     stdin().read_line(&mut user_input).expect("Error");
     let user_input: u32 = user_input.trim().parse().expect("Please enter a number");
+    validate_user_input(user_input)
+}
+
+fn validate_user_input(user_input: u32) -> u32 {
+    if user_input > 100 {
+        panic!("Value should be between 1 and 100");
+    } 
     user_input
 }
