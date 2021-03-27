66cbb2893a6dae43b22643f2e98fe786ae4bfc42
cdanansuriya <cdanansuriya.study@gmail.com>
2021-03-27 21:09:04 +0530

    add loop

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,15 @@ mod player;
 mod selector;
 
 fn main() {
-    let player_guess = player::guess();
     let machine_guess = machine::guess();
-    selector::get_decison(&player_guess, &machine_guess);
+    loop {
+        let player_guess = player::guess();
+        let win = selector::get_decison(&player_guess, &machine_guess);
+
+        if win {
+            break;
+        }
+    }
+    
+    
 }
--- src/selector.rs
+++ src/selector.rs
@@ -1,9 +1,12 @@
-pub fn get_decison(player_guess: &u32, machine_guess: &u32) {
+pub fn get_decison(player_guess: &u32, machine_guess: &u32) -> bool{
+    let mut win_status: bool = false;
     if player_guess > machine_guess {
         println!("Too High");
     } else if machine_guess > player_guess {
         println! ("Too Low");
     } else {
         println!("You Won");
+        win_status = true;
     }
+    win_status
 }
